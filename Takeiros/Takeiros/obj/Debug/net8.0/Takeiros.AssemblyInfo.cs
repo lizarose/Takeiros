98eb079fbdae25601b2ce58d93ec61c00e190f7c
Elizabeth Levinson <lizarose266@gmail.com>
2024-08-19 18:35:37 -0500

    Added sub-heading for Menu page to display categories for the menu. Set up links for those and added styling.

--- Takeiros/Takeiros/obj/Debug/net8.0/Takeiros.AssemblyInfo.cs
+++ Takeiros/Takeiros/obj/Debug/net8.0/Takeiros.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Takeiros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b9f68ea839f2235ce642a2308b9e442dbb0a27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c44ecd4b8ef44a505f47d07bf15943679ee74f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Takeiros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Takeiros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
