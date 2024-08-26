3618ddea0ce9a90b332aec9ed4b454857f7f48f6
sunsonni <sonniemnguyen@gmail.com>
2024-08-26 11:13:02 -0500

    beginnings of react router

--- Takeiros-React/src/main.jsx
+++ Takeiros-React/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDom from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+ReactDom.createRoot(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
+  document.getElementById('root')
 )
