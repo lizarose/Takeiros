a677c0f4702186a4f5478b8c3164e542d0ecb57c
Elizabeth Levinson <lizarose266@gmail.com>
2024-08-26 14:50:40 -0500

    Footer has test data for hours, address, and phone number. Added social media logos and Tequilas logo. Added some css styling

--- Takeiros-React/src/App.jsx
+++ Takeiros-React/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Navigation from './Components/NavBar/Navigation.jsx'
 import { Outlet } from 'react-router-dom'
-import Footer from "./Components/Footer/Footer.jsx"
+import Footer from "./Components/Footer/footer.jsx"
 
 function App() {
 
