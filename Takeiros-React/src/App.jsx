bda7abad7838ad83af4639d99436a9276b410705
Elizabeth Levinson <lizarose266@gmail.com>
2024-08-30 15:54:33 -0500

    Created the Header and added a font

--- Takeiros-React/src/App.jsx
+++ Takeiros-React/src/App.jsx
@@ -3,6 +3,7 @@ import Navigation from './Components/NavBar/Navigation.jsx'
 import { Outlet } from 'react-router-dom'
 import Footer from "./Components/Footer/footer.jsx"
 import HappyHourSpecials from './Components/Specials/Specials.jsx'
+import Header from './Components/Header/Header.jsx'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <>
       <Navigation />
       <Outlet />
+      <Header />
       <HappyHourSpecials />
       <Footer />
     </>
---
+++ Takeiros-React/src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+
+
+
+    return (
+            <div className='feature-photo'>
+                <div className='title'>
+                    <h1>Takeiros Mex-Food</h1>
+                </div>
+                <div className='buttons'>
+                    <a href='' className='order-online-button'>Order Online</a>
+                    <a href='' className='view-menu-button'>View Menu</a>
+                </div>
+            </div>
+    );
+}
+
+export default Header;
